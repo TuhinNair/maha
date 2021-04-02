9d29447f8eb81b1c91f7f54e3f83fc65793bd7d7
Tuhin Nair <tuhinnair444@gmail.com>
2021-04-02 18:07:06 +0530

    remove error bubbling for the time being

--- src/oracle/mod.rs
+++ src/oracle/mod.rs
@@ -1,9 +1,34 @@
 use chrono::{DateTime, Utc};
-use yahoo_finance_api as yahoo;
+use std::fmt;
+use std::{error::Error, fmt::Display};
 
-struct OHLC {}
+type OracleResult<T> = Result<T, OracleError>;
+#[derive(Debug)]
+struct OracleError {
+    message: String,
+}
+
+impl Display for OracleError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", &self.message)
+    }
+}
+
+impl Error for OracleError {}
+
+struct OHLC {
+    utc_datetime: DateTime<Utc>,
+    open: f64,
+    high: f64,
+    low: f64,
+    adjclose: f64,
+}
 trait Oracle {
-    fn new_provider() -> Self;
+    fn new_oracle() -> Self;
 
-    fn ohlc_inclusive_range(ticker: &str, start: DateTime<Utc>, end: DateTime<Utc>) -> OHLC;
+    fn ohlc_inclusive_range(
+        ticker: &str,
+        start: DateTime<Utc>,
+        end: DateTime<Utc>,
+    ) -> Result<Vec<Option<OHLC>>, OracleError>;
 }
